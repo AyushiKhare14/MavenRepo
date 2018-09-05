f253a3cf7f428f7212f72856b8c710b64ebd3e8f
Ayushi Khare <ayushi.khare03@gmail.com>
2018-09-05 18:45:04 +0530

    lets see

--- src/main/java/com/example1/App.java
+++ src/main/java/com/example1/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-System.out.println( "Hello Worlhhhhhhhhhhd!" );
+System.out.println( "Hello Pratit" );
+System.out.println( "Hello Ayushi");
     }
 }
